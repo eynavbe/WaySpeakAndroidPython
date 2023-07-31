d4bdfa80c1f694a132f896f8936829bc65cd3452
eynavbe <93534494+eynavbe@users.noreply.github.com>
2023-07-31 23:55:12 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ from ConnectFirebase import ConnectFirebase
 
 if __name__ == "__main__":
     # AnalysisRealTime('eynav.png', 'eynav.png', "mouth_open")
-    ConnectFirebase("39320201009","mouth_open", "04062023", "mouth_open5","1959_310723 ")
+    # ConnectFirebase("39320201009","mouth_open", "04062023", "mouth_open5","1959_310723 ")
 
     # MachineTraining()
     # unittest.main(module=Tests)
