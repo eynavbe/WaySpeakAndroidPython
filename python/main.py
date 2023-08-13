bb081a6b02f3f1775c96da920b76546798ca52ee
eynavbe <93534494+eynavbe@users.noreply.github.com>
2023-08-13 21:50:30 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
     # AnalysisRealTime('eynav.png', 'eynav.png', "mouth_open")
     ConnectFirebase("39320201009","mouth_open", "04062023", "mouth_open5","1959_310723 ")
 
-    Tongue('straight_tongue_out.mp4','Move_tongue_to_left')
     # MachineTraining()
     # unittest.main(module=Tests)
