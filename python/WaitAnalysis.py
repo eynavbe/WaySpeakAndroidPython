0a0d57d6d111deb405ec7adbf104361f4ef81c88
eynavbe <93534494+eynavbe@users.noreply.github.com>
2023-11-15 00:30:21 +0200

    Add files via upload

---
+++ python/WaitAnalysis.py
@@ -0,0 +1,7 @@
+class WaitAnalysis:
+    def __init__(self, clinic_name, date_start_f,formatted_date,identification_number,type_count):
+        self.clinic_name = clinic_name
+        self.date_start_f = date_start_f
+        self.formatted_date = formatted_date
+        self.identification_number = identification_number
+        self.type_count = type_count
